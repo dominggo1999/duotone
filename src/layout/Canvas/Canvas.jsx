473ea6ea9ef61f8d6bdd89372589810503e11407
dominggo1999 <arnolddominggo19@gmail.com>
2021-12-09 09:50:25 +0700

    Revert to no canvas transition animation

--- src/layout/Canvas/Canvas.jsx
+++ src/layout/Canvas/Canvas.jsx
@@ -38,7 +38,6 @@ const Canvas = () => {
     // 0.97 is to give a little bif of padding on first load
     transform: `scale(${wrapper.scale * 0.97})`,
     padding: `${wrapper.spacing}px`,
-    transition: 'width 300ms ease-in-out',
   };
 
   const imageStyle = {
